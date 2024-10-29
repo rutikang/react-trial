65e2ce8c7dfc2dbc9522e948002c21ce86ec1d7a
daniel <danielrutikanga31@gmail.com>
2024-10-30 00:06:23 +0200

    mui

--- controlled_feedback_form/src/App.js
+++ controlled_feedback_form/src/App.js
@@ -1,6 +1,9 @@
-// import logo from './logo.svg';
-import './App.css';
 import { useState } from 'react';
+
+import Box from '@mui/material/Box';
+import { Button, Divider, Typography } from '@mui/material';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 // feedback form
 
@@ -22,24 +25,32 @@ function App() {
   }
   return(
     <>
-    <h2>Feedback form</h2>
-    <form onSubmit={formSub}>
-    <label>How good is our service: {score}</label>
+    <Box sx={{alignItems:'center', justifyContent:'center', display:'flex', height:'100vh', width:'100%'}}>
+      <Box sx={{width:400, bgcolor:'#fafdff', p:2, borderRadius:2, boxShadow:3, border:'0.5px solid #d9ddde'}}>
+        <Typography sx={{fontFamily:'roboto', fontSize:25, mb:3}}>Feedback form</Typography>
+      <Divider sx={{bgcolor:'gray', mb:4}}/>
+    <form className='form' onSubmit={formSub}>
+    <label style={{marginBottom:15}} className='form-label'>How good is our service :  {score}</label>
     <input
       type='range'
       min='0'
       max='10'
       value={score}
       onChange={(e)=>setScore(e.target.value)}
+      className='form-control'
+      
     />
-    <label>Comment</label>
+    <label style={{marginBottom:15, marginTop:10}}>Comment</label>
     <textarea
       value = {comment}
       onChange={(e) => setComment(e.target.value)}
+      className='form-control'
     />
-    <button type='submit'>Submit</button>
+    <Button sx={{ bgcolor:'#1b92c4', color:'white', mb:5, mt:4,ml:1, width:'90%'}} type='submit'>Submit</Button>
     </form>
     
+    </Box>
+    </Box>
     
     </>
   )
